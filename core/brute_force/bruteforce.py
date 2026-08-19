0eb9bc282b58181e123d5a4a5390a4f1b84c87d3
yxuriii <74470850+kayden765@users.noreply.github.com>
2026-08-19 20:23:20 +0100

    Add files via upload

---
+++ core/brute_force/bruteforce.py
@@ -0,0 +1,11 @@
+"""
+Brute Force Attack Module
+Optional module for automated password brute forcing.
+"""
+
+class BruteForceAttack:
+    def __init__(self):
+        pass
+    
+    def start_bruteforce(self):
+        pass
